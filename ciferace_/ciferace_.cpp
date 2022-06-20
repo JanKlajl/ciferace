0167e3ac539d0a95edf80295ff11ed1e10721c72
Jan Klajl <jan.klajl.020@pslib.cz>
2022-06-20 14:08:47 +0200

    Přidejte soubory projektu.

---
+++ ciferace_/ciferace_.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int ciferace(int x) {
+	int m, vysledek = 0;
+	while (x > 0) {
+		m = x % 10;
+		vysledek = vysledek + m;
+		x = x / 10;
+	}
+	if (vysledek > 9) vysledek = ciferace(vysledek);
+	return vysledek;
+}
+
+int main() {
+	int hodnota = 0;
+	printf("zadej cislo: ");
+	scanf_s("%d", &hodnota);
+	printf("Vysledek je %d", ciferace(hodnota));
+}
